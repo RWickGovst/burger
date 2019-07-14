7dd19877ab3b470eac47625470692079a4010b0b
Triumph <archie200@icloud.com>
2019-07-14 16:21:03 -0500

    removed sql pass

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,7 +17,7 @@ router.get("/", function(req, res) {
 });
 
 router.post("/api/burgers", function(req, res) {
-    console.log("burger name in api: " +req.body.name);
+    // console.log("burger name in api: " +req.body.name);
   burger.create([
     "burger_name"
   ], [
@@ -31,7 +31,7 @@ router.post("/api/burgers", function(req, res) {
 router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
+//   console.log("condition", condition);
 
   burger.update({
     devoured: req.body.devoured
